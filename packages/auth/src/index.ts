c3cd9a1cd427e10af1e95688be03a6f57306dcfa
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-05-09 07:45:29 -0300

    Criando primeiras permissões

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,27 @@
+import {
+  Ability,
+  AbilityBuilder,
+  CreateAbility,
+  createMongoAbility,
+  ForcedSubject,
+} from '@casl/ability'
+
+const actions = ['manage', 'invite', 'delete'] as const
+const subjects = ['User', 'all'] as const
+
+type AppAbilities = [
+  (typeof actions)[number],
+  (
+    | (typeof subjects)[number]
+    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
+  ),
+]
+export type AppAbility = Ability<AppAbilities>
+
+const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
+const { build, can, cannot } = new AbilityBuilder(createAppAbility)
+
+can('invite', 'User')
+cannot('delete', 'User')
+
+export const ability = build()
