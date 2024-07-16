901e1b73a1f8de492e30c62873f9eaf930ca4e36
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-07-16 16:37:36 -0300

    Rota: Aceitar convite

---
+++ apps/api/src/http/routes/invites/accept-invite.ts
@@ -0,0 +1,64 @@
+import { eq } from 'drizzle-orm'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { db } from '@/db/connection'
+import { invites, members, users } from '@/db/schema'
+import { auth } from '@/http/middlewares/auth'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function acceptInvite(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/invites/:inviteId/accept',
+      {
+        schema: {
+          tags: ['invites'],
+          summary: 'Accept an invite',
+          params: z.object({
+            inviteId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+        const { inviteId } = request.params
+
+        const [invite] = await db
+          .select()
+          .from(invites)
+          .where(eq(invites.id, inviteId))
+
+        if (!invite) {
+          throw new BadRequestError('Invite not found or expired.')
+        }
+
+        const [user] = await db.select().from(users).where(eq(users.id, userId))
+
+        if (!user) {
+          throw new BadRequestError('User not found.')
+        }
+
+        if (invite.email !== user.email) {
+          throw new BadRequestError('This invite belongs to another user.')
+        }
+
+        await db.insert(members).values({
+          userId,
+          organizationId: invite.organizationId,
+          role: invite.role,
+        })
+
+        await db.delete(invites).where(eq(invites.id, inviteId))
+
+        return reply.status(204).send()
+      },
+    )
+}
