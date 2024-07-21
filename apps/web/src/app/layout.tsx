8a227514f1a35cb302db65ef5c141775e04acc3d
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-07-21 11:33:48 -0300

    Setup do shadcn/ui

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,9 +1,14 @@
 import './globals.css'
 
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter as FontSans } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import { cn } from '@/lib/utils'
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +21,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          'bg-background dark min-h-screen font-sans antialiased',
+          fontSans.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   )
 }
