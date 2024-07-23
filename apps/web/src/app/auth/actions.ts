4faa0ff91c9e4d0e5a20c182b6c469403a8ff065
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-07-23 19:23:59 -0300

    Autenticação via GitHub

---
+++ apps/web/src/app/auth/actions.ts
@@ -0,0 +1,18 @@
+'use server'
+
+import { redirect } from 'next/navigation'
+
+export async function signInWithGithub() {
+  const githubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
+
+  githubSignInURL.searchParams.set('client_id', 'Ov23liTCvLBtFqUEpr5t')
+  githubSignInURL.searchParams.set(
+    'redirect_uri',
+    'http://localhost:3000/api/auth/callback',
+  )
+  githubSignInURL.searchParams.set('scope', 'user:email')
+
+  console.log(githubSignInURL.toString())
+
+  redirect(githubSignInURL.toString())
+}
--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -2,19 +2,28 @@
 
 import { AlertTriangle, Github, Loader2 } from 'lucide-react'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
 
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
+import { ToastAction } from '@/components/ui/toast'
+import { Toaster } from '@/components/ui/toaster'
+import { useToast } from '@/components/ui/use-toast'
 import { useFormState } from '@/hooks/use-form-state'
 
+import { signInWithGithub } from '../actions'
 import { signInWithEmailAndPassword } from './actions'
 
 export function SignInForm() {
   const router = useRouter()
+  const searchParams = useSearchParams()
+  const { toast } = useToast()
+
+  const search = searchParams.get('error')
 
   const [{ success, message, errors }, handleSubmit, isPending] = useFormState(
     signInWithEmailAndPassword,
@@ -23,64 +32,91 @@ export function SignInForm() {
     },
   )
 
+  useEffect(() => {
+    if (search) {
+      toast({
+        variant: 'destructive',
+        title: 'Uh oh! Something went wrong.',
+        description: 'There was a problem with github authentication.',
+      })
+    }
+  }, [])
+
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
-      {success === false && message && (
-        <Alert variant="destructive">
-          <AlertTriangle className="size-4" />
-          <AlertTitle>Sign in failed!</AlertTitle>
-          <AlertDescription>
-            <p>{message}</p>
-          </AlertDescription>
-        </Alert>
-      )}
-
-      <div className="space-y-1">
-        <Label htmlFor="email">E-mail</Label>
-        <Input name="email" type="email" id="email" />
-
-        {errors?.email && (
-          <p className="text-xs font-medium text-rose-500 dark:text-rose-400">
-            {errors.email[0]}
-          </p>
-        )}
-      </div>
-
-      <div className="space-y-1">
-        <Label htmlFor="password">Password</Label>
-        <Input name="password" type="password" id="password" />
-        {errors?.password && (
-          <p className="text-xs font-medium text-rose-500 dark:text-rose-400">
-            {errors.password[0]}
-          </p>
+    <div>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        {success === false && message && (
+          <Alert variant="destructive">
+            <AlertTriangle className="size-4" />
+            <AlertTitle>Sign in failed!</AlertTitle>
+            <AlertDescription>
+              <p>{message}</p>
+            </AlertDescription>
+          </Alert>
         )}
 
-        <Link
-          href="/auth/forgot-password"
-          className="text-xs font-medium text-foreground hover:underline"
+        <div className="space-y-1">
+          <Label htmlFor="email">E-mail</Label>
+          <Input name="email" type="email" id="email" />
+
+          {errors?.email && (
+            <p className="text-xs font-medium text-rose-500 dark:text-rose-400">
+              {errors.email[0]}
+            </p>
+          )}
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="password">Password</Label>
+          <Input name="password" type="password" id="password" />
+          {errors?.password && (
+            <p className="text-xs font-medium text-rose-500 dark:text-rose-400">
+              {errors.password[0]}
+            </p>
+          )}
+
+          <Link
+            href="/auth/forgot-password"
+            className="text-xs font-medium text-foreground hover:underline"
+          >
+            Forgot your password?
+          </Link>
+        </div>
+
+        <Button type="submit" className="w-full" disabled={isPending}>
+          {isPending ? (
+            <Loader2 className="size-4 animate-spin" />
+          ) : (
+            'Sign in with e-mail'
+          )}
+        </Button>
+
+        <Button
+          type="button"
+          variant="link"
+          className="w-full"
+          size="sm"
+          asChild
         >
-          Forgot your password?
-        </Link>
-      </div>
-
-      <Button type="submit" className="w-full" disabled={isPending}>
-        {isPending ? (
-          <Loader2 className="size-4 animate-spin" />
-        ) : (
-          'Sign in with e-mail'
-        )}
-      </Button>
-
-      <Button type="button" variant="link" className="w-full" size="sm" asChild>
-        <Link href="/auth/sign-up">Create new account</Link>
-      </Button>
+          <Link href="/auth/sign-up">Create new account</Link>
+        </Button>
+      </form>
 
       <Separator />
 
-      <Button type="button" variant="outline" className="w-full">
-        <Github className="mr-2 size-4" />
-        Sign in with github
-      </Button>
-    </form>
+      <form action={signInWithGithub}>
+        <Button
+          type="submit"
+          variant="outline"
+          formAction={signInWithGithub}
+          className="w-full"
+        >
+          <Github className="mr-2 size-4" />
+          Sign in with github
+        </Button>
+      </form>
+
+      <Toaster />
+    </div>
   )
 }
