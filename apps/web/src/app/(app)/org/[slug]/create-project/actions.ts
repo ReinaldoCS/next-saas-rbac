6dbd1552a501219b54dd3c03b02b61fcd7967e8f
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-08-13 10:02:22 -0300

    Página: criar projeto

---
+++ apps/web/src/app/(app)/org/[slug]/create-project/actions.ts
@@ -0,0 +1,58 @@
+'use server'
+
+import { HTTPError } from 'ky'
+import { z } from 'zod'
+
+// import { createProject } from '@/http/create-project'
+
+const projectSchema = z.object({
+  name: z
+    .string()
+    .min(4, { message: 'Please, include at least 4 characters.' }),
+  description: z.string(),
+})
+
+export async function createProjectAction(data: FormData) {
+  const results = projectSchema.safeParse(Object.fromEntries(data))
+
+  if (!results.success) {
+    const errors = results.error.flatten().fieldErrors
+    return {
+      success: false,
+      message: null,
+      errors,
+    }
+  }
+
+  console.log(results.data)
+
+  const { name, description } = results.data
+
+  try {
+    // await createProject({ name, description })
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = (await error.response.json()) as { message: string }
+
+      return {
+        success: false,
+        message,
+        errors: null,
+      }
+    }
+
+    console.error(error)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  return {
+    success: true,
+    message: 'Successfully saved the project',
+    errors: null,
+  }
+}
